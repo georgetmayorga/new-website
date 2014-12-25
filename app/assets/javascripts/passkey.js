2234dc450f6a33b288b6cf23d87b3e61a9ea98c5
George Mayorga <georgetmayorga@gmail.com>
2014-12-25 18:52:12 -0500

    Site requires password to get behind facade

---
+++ app/assets/javascripts/passkey.js
@@ -0,0 +1,25 @@
+$('#show-passkey').click(function (event) {
+  this.setAttribute("style", "display: none;");
+  $('.input-area')[0].setAttribute("style", "display: inline-block;");
+});
+
+$('#passkey').keydown(function(event) {
+  if ( event.which == 13 ) {
+    if ( event.target.value === "password") {
+      $('#output-container').fadeOut(2000);
+      $(this).fadeOut(2000);
+      openSesame();
+    } else {
+      event.target.value = "";
+      $('.input-area')[0].setAttribute("style", "display: none;");
+      $('#show-passkey')[0].setAttribute("style", "display: block;");
+  };
+ }
+});
+
+function openSesame() {
+  $('body.lobby.lobby-show')[0].setAttribute("style", "background-color:black; width: 100vw; height: 100vh;");
+  $('body').html(
+    "<div class='secret-text'>This should be some words</div>"
+  );
+};
