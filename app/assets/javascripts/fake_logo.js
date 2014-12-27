30efaf60f81c47299be57f0ff54b9e9bdfc82ab9
George Mayorga <georgetmayorga@gmail.com>
2014-12-26 23:17:50 -0500

    Clicking omega open cli interface

--- app/assets/javascripts/fake_logo.js
+++ app/assets/javascripts/fake_logo.js
@@ -1,7 +1,5 @@
 function replaceLogo() {
-  var showPasskey = 'alert("hello");'
+  var showPasskey = 'parent.showPasskey();'
   $("iframe").contents().find('.central-featured-logo').after('<img alt="Logo" id="fake-logo" src="/assets/logo.png" style="margin:80px;outrine:none;" usemap="#logo"><map name="logo"><area shape="circle" coords="60,88,18" style="outline:none;" href="#" onclick=' + showPasskey + '></map>');
   $("iframe").contents().find('.central-featured-logo').remove();
 };
-
-replaceLogo();
