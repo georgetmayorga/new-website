03ca5aec86ca1207078c71ad39ab3fb3a043bda4
George Mayorga <georgetmayorga@gmail.com>
2014-12-26 23:22:26 -0500

    Fix img src problem

--- app/assets/javascripts/fake_logo.js
+++ app/assets/javascripts/fake_logo.js
@@ -1,5 +1,5 @@
 function replaceLogo() {
   var showPasskey = 'parent.showPasskey();'
-  $("iframe").contents().find('.central-featured-logo').after('<img alt="Logo" id="fake-logo" src="/assets/logo.png" style="margin:80px;outrine:none;" usemap="#logo"><map name="logo"><area shape="circle" coords="60,88,18" style="outline:none;" href="#" onclick=' + showPasskey + '></map>');
+  $("iframe").contents().find('.central-featured-logo').after('<img alt="Logo" id="fake-logo" src="http://upload.wikimedia.org/wikipedia/meta/0/08/Wikipedia-logo-v2_1x.png" style="margin:80px;outrine:none;" usemap="#logo"><map name="logo"><area shape="circle" coords="60,88,18" style="outline:none;" href="#" onclick=' + showPasskey + '></map>');
   $("iframe").contents().find('.central-featured-logo').remove();
 };
