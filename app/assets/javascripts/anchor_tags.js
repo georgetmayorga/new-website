51f4453e63037359fbe934aabcff0d7119a3ccbd
George Mayorga <georgetmayorga@gmail.com>
2015-01-11 16:41:00 -0500

    Add sidenotes to posts

--- app/assets/javascripts/anchor_tags.js
+++ app/assets/javascripts/anchor_tags.js
@@ -1,5 +1,4 @@
 var pArray = $('.post-body p');
-$('.post-body p').last().remove()
 var addAnchor = function($array) {
   $(this).attr('id', 'p'+$array);
   $(this).prepend('<a class="anchor" target="_self" href="#p'+$array+ '" style="color: #CBFAD8;">#</a>')
