d04a8e6ac2f059d48386c3c541aab303eb57b7f3
George Mayorga <georgetmayorga@gmail.com>
2015-01-11 15:28:24 -0500

    Remove extra p tag generated on posts

--- app/assets/javascripts/anchor_tags.js
+++ app/assets/javascripts/anchor_tags.js
@@ -1,4 +1,5 @@
-var pArray = $('.post-body p')
+var pArray = $('.post-body p');
+$('.post-body p').last().remove()
 var addAnchor = function($array) {
   $(this).attr('id', 'p'+$array);
   $(this).prepend('<a class="anchor" target="_self" href="#p'+$array+ '" style="color: #CBFAD8;">#</a>')
