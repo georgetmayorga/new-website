d75ffe3b21c904b816e159c58987e3d10364f29d
George Mayorga <georgetmayorga@gmail.com>
2015-01-11 12:28:57 -0500

    Add anchor tags on every paragraph

---
+++ app/assets/javascripts/anchor_tags.js
@@ -0,0 +1,7 @@
+var pArray = $('.post-body p')
+var addAnchor = function($array) {
+  $(this).attr('id', 'p'+$array);
+  $(this).prepend('<a class="anchor" target="_self" href="#p'+$array+ '" style="color: #CBFAD8;">#</a>')
+};
+
+$.each(pArray, addAnchor)
